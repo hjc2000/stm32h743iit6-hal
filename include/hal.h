c4087e1813f96ba2b957afd5755ef1900c3c69c7
hjc2000 <hjc.666@foxmail.com>
2024-05-28 09:44:47 +0800

    #include<stm32h7xx_hal_gpio.h>

--- include/hal.h
+++ include/hal.h
@@ -8,6 +8,8 @@ extern "C" {
 	#include<stm32h743xx.h>
 	#include<stm32h7xx_hal_cortex.h>
 	#include<stm32h7xx_hal_def.h>
+	#include<stm32h7xx_hal_gpio.h>
+	#include<stm32h7xx_hal_gpio_ex.h>
 	#include<stm32h7xx_hal_rcc.h>
 	#include<stm32h7xx_hal_rcc_ex.h>
 	#include<stm32h7xx_hal_uart.h>
