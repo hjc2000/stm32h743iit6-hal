ba428e7affbd3530ab8cd0e70793d4b1dd3ccbf9
hjc2000 <hjc.666@foxmail.com>
2024-05-28 09:40:43 +0800

    hal.h

---
+++ include/hal.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+	#endif // __cplusplus
+
+	#include"stm32h7xx_hal.h"
+	#include<core_cm7.h>
+	#include<stm32h743xx.h>
+	#include<stm32h7xx_hal_cortex.h>
+	#include<stm32h7xx_hal_def.h>
+	#include<stm32h7xx_hal_rcc.h>
+	#include<stm32h7xx_hal_rcc_ex.h>
+	#include<stm32h7xx_hal_uart.h>
+	#include<stm32h7xx_ll_fmac.h>
+
+
+	#ifdef __cplusplus
+}
+#endif // __cplusplus
