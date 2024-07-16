136382941cd3447baa4873de5f1411a62e327e1a
hjc2000 <hjc.666@foxmail.com>
2024-07-16 10:40:32 +0800

    void SystemInit(void)

--- include/hal.h
+++ include/hal.h
@@ -1,22 +1,22 @@
 #pragma once
 #ifdef __cplusplus
-extern "C" {
-	#endif // __cplusplus
+extern "C"
+{
+#endif // __cplusplus
 
-	#include"stm32h7xx_hal.h"
-	#include<core_cm7.h>
-	#include<stm32h743xx.h>
-	#include<stm32h7xx_hal_cortex.h>
-	#include<stm32h7xx_hal_def.h>
-	#include<stm32h7xx_hal_gpio.h>
-	#include<stm32h7xx_hal_gpio_ex.h>
-	#include<stm32h7xx_hal_rcc.h>
-	#include<stm32h7xx_hal_rcc_ex.h>
-	#include<stm32h7xx_hal_uart.h>
-	#include<stm32h7xx_ll_fmac.h>
-	#include<stm32h7xx_ll_fmc.h>
+#include "stm32h7xx_hal.h"
+#include <core_cm7.h>
+#include <stm32h743xx.h>
+#include <stm32h7xx_hal_cortex.h>
+#include <stm32h7xx_hal_def.h>
+#include <stm32h7xx_hal_gpio.h>
+#include <stm32h7xx_hal_gpio_ex.h>
+#include <stm32h7xx_hal_rcc.h>
+#include <stm32h7xx_hal_rcc_ex.h>
+#include <stm32h7xx_hal_uart.h>
+#include <stm32h7xx_ll_fmac.h>
+#include <stm32h7xx_ll_fmc.h>
 
-
-	#ifdef __cplusplus
+#ifdef __cplusplus
 }
 #endif // __cplusplus
